dc4c744d88566f5169e1f3c5a635a17a35a87739
jgutierrez-psdc <49690082+jgutierrez-psdc@users.noreply.github.com>
2019-05-22 12:08:59 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+#class Marks(models.Model):
+ #   _name_ = 'marks'
+#
+ #   student_id = fields.Many2one('students','Student')
+  #  course_id = fields.Many2one('courses','Courses')
+   # exam1 = fields.Float('Exam1')
+    #exam2 = fields.Float('Exam2')
+    #extra = fields.Float('Discipline & Presence')
+    #average  = fields.Float('Average Mark', compute='_get_avg_mark', store=True)
